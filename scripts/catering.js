563a11936625a767ef235a2dafad1b8dc7e2b75e
Abhimanyu <lp.abhimanyu@gmail.com>
2023-01-20 23:10:04 +0530

    Day-4 Work Update

--- scripts/catering.js
+++ scripts/catering.js
@@ -20,3 +20,48 @@ btnClose.addEventListener("click",()=>{
     btnOpen.style.display = "block"
     })
 
+//Catching & Appending Menu Data to Menu element.
+
+let menuEl = document.getElementById("cat-menu");
+let cartData = JSON.parse(localStorage.getItem("cart"))
+if(cartData==null){
+    cartData=[];
+}
+
+let menuData = [{image:"./media/Catering24Piece.png", name:"24 Piece Meal", price:"69"},
+{image:"./media/CateringChickenMeals_48PcMeal.png", name:"48 Piece Meal", price:"69"},
+{image:"./media/Catering24PieceMixedChickenTortillas.png", name:"24 Piece Chicken & Tortillas", price:"69"},
+{image:"./media/3CourseCatering_24PC.png", name:"3-Course 24 Piece Meal", price:"69"},
+{image:"./media/Catering48 PieceChicken.png", name:"3-Course 48 Piece Meal", price:"69"}];
+
+
+Display(menuData);
+
+//Display function
+
+function Display(data){
+    menuEl.innerHTML=null;
+    data.forEach((element)=>{
+        let card = document.createElement("div");
+
+        let image = document.createElement("img");
+        image.src = element.image;
+
+        let name = document.createElement("h2");
+        name.innerText = element.name;
+
+        let price = document.createElement("h4");
+        price.innerText = element.price+"$";
+
+        let button = document.createElement("button")
+        button.innerText = "ADD";
+
+        button.addEventListener("click",()=>{
+            cartData.push(element);
+            localStorage.setItem("cart",JSON.stringify(cartData))
+        })
+
+        card.append(image, name, price, button)
+        menuEl.append(card);
+    })
+}
